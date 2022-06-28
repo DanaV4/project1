2c480fd377f81e086b0f5eec0fa93db8ace42e47
DanaV4 <108320867+DanaV4@users.noreply.github.com>
2022-06-28 10:16:12 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,55 @@
+
+package testemployee;
+
+public class Employee {
+    
+ private String fristName;
+    private String lastName;
+    private double salary;
+    private int id;
+       
+      Employee(){
+          
+      }
+      Employee(String fristName,String lastName,double salary,int id){
+          this.fristName=fristName;
+          this.lastName=lastName;
+          this.salary=salary;
+          this.id=id;
+      }
+      public void setFristName(String fristNmae){
+          this.fristName=fristNmae;
+          
+      }
+      public String getFristName(){
+          return this.fristName;
+      }
+      public void setLastName(String lastName){
+          this.lastName=lastName;
+      }
+      public String getLastName(){
+          return this.lastName;
+      }
+      public void setsalary(double salary){
+          this.salary=salary;
+      }
+      public double getsalary(){
+          return this.salary;
+      }
+      public void setid(int id){
+          this.id=id;
+      }
+      public int getid(){
+          return this.id;
+      }
+
+    @Override
+    public String toString() {
+        return   "fristName is : " + fristName + "\nlastName is : " + lastName + 
+                "\nsalary for employee is : " + salary + "\nid is : " + id ;
+    }
+   
+   
+  
+}
+
---
+++ TestEmployee.java
@@ -0,0 +1,11 @@
+
+package testemployee;
+
+public class TestEmployee {
+
+    public static void main(String[] args) {
+        Employee Employee1=new Employee("Ali","Alahamde",12000.0,1223);
+        System.out.println(Employee1.toString());
+    }
+    
+}
