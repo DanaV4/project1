893f26e6cb47a167e500d845bd9f29815e6a9754
Dana <108320867+DanaV4@users.noreply.github.com>
2022-07-02 10:11:23 +0300

    Update TestEmployee.java

--- TestEmployee.java
+++ TestEmployee.java
@@ -5,7 +5,9 @@ public class TestEmployee {
 
     public static void main(String[] args) {
         Employee Employee1=new Employee("Ali","Alahamde",12000.0,1223);
+         double x=  Employee_1.calSalary(50, 1000);
         System.out.println(Employee1.toString());
+          System.out.println("the salary after overtime hours :  " + x);
     }
     
 }
